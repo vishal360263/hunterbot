03eed37b9c0632b8cfd8609e7ff43abb5e971b32
vishal360263 <viraljacky.540kvsrodelhi@gmail.com>
2026-05-17 09:10:34 +0530

    Improve targeting and movement for bot behavior
    Enhance bot's targeting and movement logic with last known position tracking.

--- bot1.js
+++ bot1.js
@@ -117,16 +117,43 @@ function createBot() {
         }
       }
 
-      const target = bot.players["leo4200"]
+     const target = bot.players["leo4200"]
 
-      if (!target || !target.entity) {
-        bot.pvp.stop()
-        bot.pathfinder.setGoal(null)
-        return
-      }
+// PLAYER VISIBLE
+if (target && target.entity) {
+
+  lastKnownPos = target.entity.position.clone()
+
+} else {
+
+  // FOLLOW LAST KNOWN COORDS
+  if (lastKnownPos) {
+
+    const goal = new goals.GoalNear(
+      lastKnownPos.x,
+      lastKnownPos.y,
+      lastKnownPos.z,
+      2
+    )
+
+    bot.pathfinder.setGoal(goal)
+  }
+
+  return
+}
 
       const distance = bot.entity.position.distanceTo(target.entity.position)
       const yDiff = target.entity.position.y - bot.entity.position.y
+      // WATER CHASE
+if (bot.entity.isInWater) {
+
+  bot.setControlState('jump', true)
+  bot.setControlState('forward', true)
+
+} else {
+
+  bot.setControlState('jump', false)
+}
 
       verticalMode = yDiff > 1.2
 
@@ -280,6 +307,7 @@ setInterval(() => {
 bot.once('spawn', () => {
 
   console.log("Bot first spawn → giving kit")
+  defaultMove.canSwim = true
 
   setTimeout(giveKit, 2000)
 })
